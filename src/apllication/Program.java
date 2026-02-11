241f68e40ee1d525dda547ea8d90fde3b70a9512
Lucas Marafigo <marafigo.lucas16@gmail.com>
2026-02-11 10:27:20 -0300

    findById implementation

--- src/apllication/Program.java
+++ src/apllication/Program.java
@@ -10,13 +10,10 @@ import java.util.Date;
 public class Program {
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "books");
+      SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        Seller seller = new Seller(1, "Bob", "bob@gmail.com", new Date(), 3000.00, obj );
+      Seller seller = sellerDao.findById(3);
 
-        SellerDao sellerDao = DaoFactory.createSellerDao();
-
-        System.out.println(obj);
         System.out.println(seller);
 
 
