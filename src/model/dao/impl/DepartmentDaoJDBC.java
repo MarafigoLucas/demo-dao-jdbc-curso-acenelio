0da18bfdd55663f954dd18c1915ba7a505dc8a69
Lucas Marafigo <marafigo.lucas16@gmail.com>
2026-02-24 21:23:02 -0300

    My own implementation of Department update

--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -52,7 +52,19 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public void update(Department obj) {
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement("UPDATE Department "
+                            +"SET Name = ?"
+                            +"WHERE id = ?");
 
+            st.setString(1, obj.getName());
+            st.setInt(2,obj.getId());
+            st.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
